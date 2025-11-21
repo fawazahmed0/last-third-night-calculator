88a0dd0c6e92f99f768d7f04e620189cf3b48631
Fawaz Ahmed <fawazahmed0@hotmail.com>
2025-11-21 23:55:18 +0530

    Create copy-files.js

---
+++ copy-files.js
@@ -0,0 +1,16 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const directoryToCopyFrom = path.join(__dirname, 'repo', 'dist')
+const directoryToCopyTo = path.join(__dirname, 'docs')
+
+async function main(){
+    await fs.mkdir(directoryToCopyFrom, {recursive: true})
+    await fs.mkdir(directoryToCopyTo, {recursive: true})
+
+    const filesToCopy = ['common.js', 'feedback.json', 'form.html', 'form.js']
+    for (const file of (await fs.readdir(directoryToCopyFrom)).filter(f => filesToCopy.includes(f))) 
+        await fs.copyFile(path.join(directoryToCopyFrom, file), path.join(directoryToCopyTo, file))
+    
+}
+main()
