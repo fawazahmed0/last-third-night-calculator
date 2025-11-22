729a32629a7abdc4294b4f1489f0620d8b5b4758
Fawaz Ahmed <fawazahmed0@hotmail.com>
2025-11-23 03:09:38 +0530

    use repo path from config.ini

--- copy-files-from-repo.js
+++ copy-files-from-repo.js
@@ -1,14 +1,16 @@
 const fs = require('fs/promises')
 const path = require('path')
 
-const directoryToCopyFrom = path.join(__dirname, 'repo')
+const pathToConfig = path.join(__dirname, 'config.ini')
 const directoryToCopyTo = path.join(__dirname, 'code')
 
-async function main(){
-    await fs.mkdir(directoryToCopyFrom, {recursive: true})
-    await fs.mkdir(directoryToCopyTo, {recursive: true})
+// Files to copy from the repo
+const filesToCopy = ['common.js', 'form.html', 'form.js']
 
-    const filesToCopy = ['common.js', 'form.html', 'form.js']
+async function main(){
+    const config = JSON.parse(await fs.readFile(pathToConfig, 'utf-8'))
+    const directoryToCopyFrom = process.env.CI ? path.join(__dirname, 'repo') : path.join(__dirname, ...config.repoPath)
+
     for (const file of (await fs.readdir(directoryToCopyFrom)).filter(f => filesToCopy.includes(f))) 
         await fs.copyFile(path.join(directoryToCopyFrom, file), path.join(directoryToCopyTo, file))
     
