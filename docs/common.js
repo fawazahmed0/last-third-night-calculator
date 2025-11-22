dca41c4bfe29dd47c048a0e4953bf97227ddb1e7
Fawaz Ahmed <fawazahmed0@hotmail.com>
2025-11-23 03:49:22 +0530

    run build

---
+++ docs/common.js
@@ -0,0 +1,261 @@
+const commonKey = 'sRCZJbf8ltHkmkqh/Gjqhk7tfP6L0Pq9S0dX+chXNzA='
+
+function normalizeEmail(email) {
+    if(!email.includes("@") || email.length <3)
+        throw "Invalid email address"
+    let emailArr = email.trim().split("@")
+    return `${emailArr.slice(0, -1).join("@")}@${emailArr.at(-1).toLowerCase()}`
+}
+
+function subsetJSON(json, keysToKeep) {
+    let newjson = {}
+    keysToKeep.forEach(e => newjson[e] = json[e])
+    return newjson
+}
+
+async function encryptData(key, iv, data) {
+    const encoder = new TextEncoder();
+    const cipherText = await crypto.subtle.encrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        key,
+        encoder.encode(data)
+    );
+    return new Uint8Array(cipherText).toBase64();
+}
+
+async function decryptData(key, iv, cipherTextBase64) {
+    const decoder = new TextDecoder();
+    const plainBuffer = await crypto.subtle.decrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        key,
+        Uint8Array.fromBase64(cipherTextBase64)
+    );
+    return decoder.decode(plainBuffer);
+}
+
+async function importBase64Key(base64String) {
+    return await crypto.subtle.importKey(
+        "raw",
+        Uint8Array.fromBase64(base64String),
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"],
+    );
+}
+
+// privateKeyValue can be base64 or Uint8Array or ArrayBuffer
+async function getX25519Keys(privateKeyValue) {
+
+    let pkcsKeyArray = await convertRawX25519KeyToPKCS(privateKeyValue)
+
+    let privateKey = await crypto.subtle.importKey("pkcs8", pkcsKeyArray, { name: "X25519" }, true, ["deriveKey"])
+
+    let jwkKey = await crypto.subtle.exportKey("jwk", privateKey)
+
+    let { d: privateRaw, x: publicRaw } = jwkKey
+    // Remove private key variables to create public key
+    delete jwkKey.d;
+    jwkKey.key_ops = [];
+    let publicKey = await crypto.subtle.importKey("jwk", jwkKey, { name: "X25519" }, true, [])
+
+    privateRaw = Uint8Array.fromBase64(privateRaw, { alphabet: "base64url" })
+    publicRaw = Uint8Array.fromBase64(publicRaw, { alphabet: "base64url" })
+
+    let privateBase64 = privateRaw.toBase64()
+    let publicBase64 = publicRaw.toBase64()
+
+    return { privateRaw, publicRaw, privateBase64, publicBase64, privateKey, publicKey };
+
+}
+
+async function convertRawX25519KeyToPKCS(privateKey, base64Options) {
+    // Convert to raw if base64
+    if (typeof privateKey === 'string' || privateKey instanceof String)
+        privateKey = Uint8Array.fromBase64(privateKey, base64Options)
+
+    // We use this key to know PKCS header data
+    const tempKeys = await crypto.subtle.generateKey({ name: "X25519" }, true, ["deriveKey"])
+
+    let pkcsKey = await crypto.subtle.exportKey("pkcs8", tempKeys.privateKey)
+    let jwkKey = await crypto.subtle.exportKey("jwk", tempKeys.privateKey)
+
+    let pkcsArray = new Uint8Array(pkcsKey)
+    let jwkArray = Uint8Array.fromBase64(jwkKey.d, { alphabet: "base64url" })
+    // Replace the private key part in the PKCS array with our private key to get PKCS format
+    let pkcsString = pkcsArray.join(',').replace(jwkArray.join(','), privateKey.join(','))
+
+    return new Uint8Array(pkcsString.split(','))
+}
+
+async function getAESKeyFromX25519(ourPrivateKey, theirPublicKey) {
+    return await crypto.subtle.deriveKey({ name: "X25519", public: theirPublicKey },
+        ourPrivateKey,
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"])
+}
+
+function showSpinningWheel(selector, position) {
+  if (!document.body.contains(document.querySelector('#spinningwheel')) ) {
+    document.querySelector(selector).insertAdjacentHTML(position,`<div  id="spinningwheel">
+    <div class="text-center">
+      <div class="spinner-border m-5" role="status">
+      </div>
+      </div>
+      </div>
+      `)
+  }
+}
+
+function removeSpinningWheel() {
+  if (document.body.contains(document.querySelector('#spinningwheel')) )
+  document.querySelector('#spinningwheel').remove()
+}
+
+function showModal(title, body) {
+  let myModal = new bootstrap.Modal(document.getElementById('modaldiv'))
+  document.querySelector('#modaldiv .modal-title').innerHTML = title
+  document.querySelector('#modaldiv .modal-body').innerHTML = body
+  myModal.show()
+}
+
+// Call this function after loading the module in nodejs or after domcontentloaded in ready func in browser
+async function initializeGlobalVariables() {
+    globalThis.commonCryptoKey = await crypto.subtle.importKey("raw", Uint8Array.fromBase64(commonKey), "AES-GCM", true, ["encrypt", "decrypt"]);
+}
+
+
+// Refer https://bitwarden.com/help/bitwarden-security-white-paper
+async function getKeys(email, password) {
+
+    let masterKey = await getMasterKey(password, email);
+    let { data: dataCryptoKey, wrap: wrapCryptoKey } = await getDataAndWrapKeys(masterKey, ["data", "wrap"])
+
+    let dataBase64Key = await getBase64KeyString(dataCryptoKey);
+    let wrapBase64Key = await getBase64KeyString(wrapCryptoKey);
+
+    let uuidCryptoKey = await getMasterKey(await crypto.subtle.exportKey("raw", masterKey), password)
+    let uuidBase64 = await getBase64KeyString(uuidCryptoKey)
+
+    return { uuidBase64, dataBase64Key, wrapBase64Key, dataCryptoKey, wrapCryptoKey }
+
+}
+
+
+async function getBase64KeyString(key) {
+    return new Uint8Array(await crypto.subtle.exportKey("raw", key)).toBase64();
+}
+
+async function getMasterKey(password, salt) {
+    const enc = new TextEncoder();
+    if (isString(password))
+        password = enc.encode(password);
+    if (isString(salt))
+        salt = enc.encode(salt);
+    const masterPassword = await crypto.subtle.importKey(
+        "raw",
+        password,
+        "PBKDF2",
+        false,
+        ["deriveBits", "deriveKey"]
+    );
+    const masterKey = await crypto.subtle.deriveKey(
+        {
+            name: "PBKDF2",
+            salt,
+            iterations: 600_000,
+            hash: "SHA-256",
+        },
+        masterPassword,
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"],
+    );
+    return masterKey
+}
+
+async function exportImportKey(key, algorithm, extractable = true, usages) {
+    const rawKey = await crypto.subtle.exportKey("raw", key);
+    return await crypto.subtle.importKey("raw", rawKey, algorithm, extractable, usages);
+}
+// Data key to encrypt data
+// Wrap key to encrypt other users data key
+async function getDataAndWrapKeys(masterKey, infos) {
+
+    masterKey = await exportImportKey(masterKey, "HKDF", false, ["deriveKey"]);
+    let keyObject = {};
+
+    for (let value of infos) {
+        keyObject[value] = await crypto.subtle.deriveKey(
+            {
+                name: "HKDF",
+                salt: new Uint8Array(),
+                info: new TextEncoder().encode(value),
+                hash: "SHA-256",
+            },
+            masterKey,
+            { name: "AES-GCM", length: 256 },
+            true,
+            ["encrypt", "decrypt"],
+        );
+    }
+
+
+    return keyObject
+
+}
+
+function isString(value) {
+    return typeof value === "string" || value instanceof String;
+}
+
+async function decompressBlobToJSON(blob) {
+  const ds = new DecompressionStream("gzip");
+  const decompressedStream = blob.stream().pipeThrough(ds);
+  return await new Response(decompressedStream).json();
+}
+
+// Recursively replace nested array
+function recursiveReplaceArray(arr, jsonMap) {
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]))
+           arr[i] = recursiveReplaceArray(arr[i], jsonMap)
+        else
+            arr[i] = arr[i] in jsonMap ? jsonMap[arr[i]] : arr[i]
+    }
+return arr
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+
+
+if (typeof process !== "undefined" && process.versions != null && process.versions.node != null)
+    module.exports = {
+        subsetJSON,
+        normalizeEmail,
+        encryptData,
+        decryptData,
+        importBase64Key,
+        convertRawX25519KeyToPKCS,
+        getX25519Keys,
+        getAESKeyFromX25519,
+        initializeGlobalVariables,
+        getKeys,
+        isString,
+        getBase64KeyString,
+        exportImportKey,
+        decompressBlobToJSON,
+        recursiveReplaceArray,
+        getRandomInt,
+    }
---
+++ docs/script.js
@@ -0,0 +1,163 @@
+function getPartNightAndHijri(fajrhour, fajrmin, magribhour, magribmin) {
+    let magribDate = new Date()
+    let fajrDate = new Date(magribDate)
+    magribDate.setHours(magribhour)
+    magribDate.setMinutes(magribmin)
+    fajrDate.setHours(fajrhour)
+    fajrDate.setMinutes(fajrmin)
+
+    let diff;
+    // Calculate the time from fajr to magrib and subtract it with 1 day in millis to get the time between magrib and fajr
+    if (magribDate > fajrDate)
+        diff = 86400000 - (magribDate - fajrDate)
+    // Calculate the time from magrib to fajr as fajrDate is bigger than magribDate
+    else
+        diff = fajrDate - magribDate
+    let twothirdNight = new Date(magribDate.getTime() + (diff * 2 / 3))
+    let halfNight = new Date(magribDate.getTime() + (diff * 1 / 2))
+
+    let todayHijri = new Date();
+    // if sun has already set, then increase the date by 1 day
+    if(magribDate<todayHijri)
+    todayHijri.setDate(todayHijri.getDate() + 1)
+
+    return [halfNight, twothirdNight, todayHijri]
+
+}
+
+globalThis.setPartNightAndHijri = function () {
+    if (document.querySelector('#fajrtime').value != '' && document.querySelector('#magribtime').value != '') {
+        showInvitationIfEligible()
+        let [halfNight, twothirdNight, hijriDate] = getPartNightAndHijri(...document.querySelector('#fajrtime').value.split(':'), ...document.querySelector('#magribtime').value.split(':'))
+
+        document.querySelector('#lastthird').innerText = twothirdNight.toLocaleString('default', { hour: 'numeric', minute: 'numeric' })
+        document.querySelector('#midnight').innerText = halfNight.toLocaleString('default', { hour: 'numeric', minute: 'numeric' }) 
+        
+        let dateoffset = parseInt(document.querySelector('#dateoffset').value || '0')
+        document.querySelector('#datepencil').removeAttribute("hidden") 
+        hijriDate.setDate(hijriDate.getDate() + dateoffset)
+        let hijriString = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'numeric',day: 'numeric',calendar:'islamic'}).format(hijriDate)
+        let hijriArr = hijriString.split(' ')[0].split('/')
+        document.querySelector('#hijridate').innerText =  hijriArr[1]+ ' ' + IslamicMonths[hijriArr[0]-1]   + ' ' + hijriArr[2] + ' AH'
+        let todayDay = (new Date()).getDay()
+        let calendarDate = new Date()
+        calendarDate.setDate(hijriArr[1])
+        // zero indexed month
+        calendarDate.setMonth(hijriArr[0]-1)
+        while(true){
+            calendarDate.setFullYear(calendarDate.getFullYear()-1)
+            if(calendarDate.getDay() == todayDay)
+                break
+        }
+        document.querySelector('#dateInput').value = calendarDate.toLocaleDateString('en-CA')
+        localStorage.setItem('dateoffset', dateoffset)
+        // new Intl.DateTimeFormat(['islamic','islamic-tbla','islamic-umalqura','islamic-rgsa','islamic-civil'].map(e=>'en-u-ca-'+e),{dateStyle:'long' }).format(hijriDate)
+        }
+}
+
+globalThis.autoDetectWithCoords = function () {
+    if ('geolocation' in navigator) {
+        let autoDetectBtn = document.querySelector('#autodetectbtn')
+        autoDetectBtn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Auto Detect`
+        autoDetectBtn.disabled = true;
+        navigator.geolocation.getCurrentPosition(pos => {
+            var solar = new SolarCalc(new Date(), pos.coords.latitude, pos.coords.longitude);
+            let fajrDate = solar.astronomicalDawn;
+            let magribDate = solar.sunset;
+
+            document.querySelector('#fajrtime').value = fajrDate.toLocaleString('default', { hour: 'numeric', minute: 'numeric', hour12: false })
+            document.querySelector('#magribtime').value = magribDate.toLocaleString('default', { hour: 'numeric', minute: 'numeric', hour12: false })
+
+            setPartNightAndHijri()
+
+            autoDetectBtn.innerHTML = `Auto Detect`
+            autoDetectBtn.disabled = false;
+
+        }, err => { autoDetectBtn.innerHTML = `Auto Detect`; autoDetectBtn.disabled = false; })
+    }
+
+}
+
+// Check if we should show the invitation (after 3 days of usage in last 20 days)
+function showInvitationIfEligible() {
+    const visits = JSON.parse(localStorage.getItem('thirdNightVisits') || '[]');
+
+    const last20Days = new Date();
+    last20Days.setDate(last20Days.getDate() - 20);
+
+    // Filter visits in last 20 days
+    const recentVisits = [...new Set(visits.filter(visit => new Date(visit) > last20Days))]
+
+    // If user has visited 3 or more times in last 20 days
+    // Don't show if dialog shown today or user has opted to never show again
+    if (recentVisits.length >= 3 && localStorage.getItem('thirdNightSkipToday') !== new Date().toISOString().slice(0, 10) && localStorage.getItem('thirdNightInvitationResponse') !== 'never') {
+        localStorage.setItem('thirdNightInvitationEligible', true);
+        showInvitation()
+    }
+
+    let hijriString = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'numeric', day: 'numeric', calendar: 'islamic' }).format(new Date())
+    let hijriDate = new Date()
+    let [hijriMonth, hijriDay] = hijriString.split('/')
+    hijriDate.setMonth(hijriMonth - 1)
+    hijriDate.setDate(hijriDay)
+    let fromHijri = new Date()
+    fromHijri.setMonth(8 - 1)
+    fromHijri.setDate(20)
+    let toHijri = new Date()
+    toHijri.setMonth(10 - 1)
+    toHijri.setDate(10)
+
+    const isRamadan = fromHijri < hijriDate && hijriDate < toHijri
+
+    // Record this visit
+    // Don't record if this is ramadan month or 10 days before or after ramadan. Everyone is religious this time
+    if (!isRamadan)
+        visits.push(new Date().toISOString().slice(0, 10)); // Store only the date part
+    localStorage.setItem('thirdNightVisits', JSON.stringify(visits));
+}
+
+function showInvitation() {
+    const invitationCollapse = new bootstrap.Collapse(document.getElementById('invitationCollapse'), {
+        show: true
+    });
+}
+
+globalThis.handleInvitation = function (response) {
+    const invitationCollapse = new bootstrap.Collapse(document.getElementById('invitationCollapse'), {
+        hide: true
+    });
+
+    // Don't show again today, if any button is pressed
+    localStorage.setItem('thirdNightSkipToday', new Date().toISOString().slice(0, 10));
+
+    switch (response) {
+        case 'yes':
+            window.open("form")
+            break;
+        case 'never':
+            localStorage.setItem('thirdNightInvitationResponse', 'never');
+            break;
+    }
+}
+
+// Register Service worker for Add to Home Screen option to work
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register(new URL('service-worker.js', import.meta.url)) }
+
+function ready() {
+    let params = new URLSearchParams(document.location.search);
+    if(params.get("date"))
+    document.querySelector('#datecontainerspan').removeAttribute("hidden") 
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    document.querySelector('#dateoffset').value = localStorage.getItem('dateoffset') || '0'
+
+
+}
+
+
+import SolarCalc from 'https://cdn.jsdelivr.net/npm/@fawazahmed0/solar-calc/+esm'
+var IslamicMonths = ["Muharram","Safar","Rabiʻ I","Rabiʻ II","Jumada I","Jumada II","Rajab","Shaʻban","Ramadan","Shawwal","Dhuʻl-Qiʻdah","Dhuʻl-Hijjah"]
+document.addEventListener("DOMContentLoaded", ready);
