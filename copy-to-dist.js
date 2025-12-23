582e58b0ef8d11e4af5ccf30296e0439bcb5b2a7
Fawaz Ahmed <fawazahmed0@hotmail.com>
2025-12-23 16:16:41 +0530

    remove dependency

--- copy-to-dist.js
+++ copy-to-dist.js
@@ -1,14 +1,12 @@
 const fs = require('fs/promises')
 const path = require('path')
 
+const {minifyFile, listDirRecursive} = require('./utilities')
+
 const sourceDir = path.join(__dirname, 'code')
 const distDir = path.join(__dirname, 'docs')
-const pathToConfig = path.join(__dirname, 'config.ini')
 
 async function main() {
-  const pathToRepoDir = process.env.CI ? path.join(__dirname, 'repo') : path.join(__dirname, ...JSON.parse(await fs.readFile(pathToConfig)).repoPath)
-  const {minifyFile, listDirRecursive} = require(path.join(pathToRepoDir, 'utilities.js'))
-
   await fs.rm(distDir, { recursive: true, force: true })
   await fs.cp(sourceDir, distDir, { recursive: true })
 
